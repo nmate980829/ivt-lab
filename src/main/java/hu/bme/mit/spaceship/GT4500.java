a155778f40b99b7eef2cd29f4b2ad40c54039985
Máté Nagy <nmate980829@gmail.com>
2021-04-17 12:47:36 +0200

    create merge conflict2

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -77,6 +77,7 @@ public class GT4500 implements SpaceShip {
         break;
 
       case ALL:
+        //conflict
         // try to fire both of the torpedo stores
         if (! primaryTorpedoStore.isEmpty()) {
           firingSuccess = primaryTorpedoStore.fire(1);
