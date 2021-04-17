a99846e08a672cdc4c6795272478323ee265f32a
Máté Nagy <nmate980829@gmail.com>
2021-04-17 12:20:43 +0200

    add feature torpedo firing mode all to gt4500

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -78,8 +78,15 @@ public class GT4500 implements SpaceShip {
 
       case ALL:
         // try to fire both of the torpedo stores
-        //TODO implement feature
-
+        if (! primaryTorpedoStore.isEmpty()) {
+          firingSuccess = primaryTorpedoStore.fire(1);
+          wasPrimaryFiredLast = true;
+        }
+        if (! secondaryTorpedoStore.isEmpty()) {
+          firingSuccess = firingSuccess || secondaryTorpedoStore.fire(1);
+          wasPrimaryFiredLast = false;
+        }
+        // if both of the stores are empty, nothing can be done, return failure
         break;
     }
 
